9b7c9bc0221c60219e6d83685b83cf8d13bf1250
Daniyal Shafi <dshafi@usc.edu>
2018-09-15 00:27:19 -0700

    Adding Files to this repository

---
+++ src/PA3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "bmplib.h"
+#include <cmath>
+#inlcude <cstdlib>
+using namespace std;
+
+int readRGBBMP(char filename[], unsigned char inputImage[][256][3]);
+int writeRGBBMP(char filename[], unsigned char outputImage[][256][3]);
+
+int main(int argc, char *argv[]){
+	
+	
+	
+	
+	return 0;
+}
